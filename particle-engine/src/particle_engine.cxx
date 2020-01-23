55b8c2a66834c9c09bad40d455938a5b81b0f521
Almaz Kinziabulatov <auqolaq@gmail.com>
2020-01-23 09:09:34 +0200

    Particle engine in progress.

---
+++ particle-engine/src/particle_engine.cxx
@@ -0,0 +1,288 @@
+#include <cmath>
+
+//#include "app.h"
+#include "particle_engine.hxx"
+
+
+namespace
+{
+    auto constexpr kPI = 3.14159265358979323846f;
+}
+
+namespace app
+{
+    particle_engine::particle_engine()
+    {
+        stop_workers = false;
+
+        for (auto &&index : acquired_indices)
+            index.clear();
+
+        acquired_indices.at(0).test_and_set();
+
+        frames_data.resize(FRAMES_COUNT);
+
+        auto const workers_count = std::max(std::thread::hardware_concurrency() - 1u, 1u);
+
+        barrier = std::make_unique<utility::barrier>(workers_count);
+
+        workers.reserve(workers_count);
+
+        for (auto worker_index = 0u; worker_index < workers_count; ++worker_index)
+            workers.emplace_back(&particle_engine::worker_object, this, worker_index);
+    }
+
+    particle_engine::~particle_engine()
+    {
+        stop_workers = true;
+        barrier->drop();
+
+        for (auto &worker : workers)
+            worker.join();
+    }
+
+    void app::particle_engine::update(std::int64_t dt)
+    {
+        global_timer.fetch_add(dt);
+    }
+
+    void particle_engine::render()
+    {
+        auto frame_index = acquire_frame_index(shared_frame_read_index.load());
+
+        auto &&frame_data = frames_data.at(frame_index);
+
+        for (auto i = 0u; i < frame_data.particles_count; ++i) {
+            auto &&particle = frame_data.particles.at(i);
+            auto &&position = particle.position;
+            auto &&color = particle.color;
+
+            app::drawPoint(position.at(0), position.at(1), color.at(0), color.at(1), color.at(2), color.at(3));
+        }
+
+        release_frame_index(frame_index);
+    }
+
+    void particle_engine::spawn_effect(app::vec2 &&position, app::vec4 &&color)
+    {
+        auto frame_index = acquire_available_frame_index();
+
+        auto &&frame_data = frames_data.at(frame_index);
+
+        frame_data.effect.count = PER_EFFECT_PARTICLES_COUNT;
+        frame_data.effect.position = std::move(position);
+        frame_data.effect.color = std::move(color);
+
+        release_frame_index(frame_index);
+    }
+
+    void particle_engine::worker_object(std::uint32_t worker_index)
+    {
+        auto frame_read_index = 0u, frame_write_index = 1u;
+
+        barrier->wait();
+
+        app::worker_context worker_context{worker_index};
+
+        while (!stop_workers) {
+            update_worker_time_points(worker_context);
+
+            if (worker_context.dt == 0) {
+                std::this_thread::sleep_for(UPDATE_PERIOD - std::chrono::milliseconds{worker_context.dt});
+                continue;
+            }
+
+            frame_write_index = shared_frame_write_index.load();
+            
+            worker_context.read_frame = &frames_data.at(frame_read_index);
+            worker_context.write_frame = &frames_data.at(frame_write_index);
+
+            auto &&read_frame = *worker_context.read_frame;
+            auto &&write_frame = *worker_context.write_frame;
+
+            add_particles(worker_context);
+
+            process_particles(worker_context);
+
+            frame_read_index = frame_write_index;
+
+            if ((++idle_workers) == workers.size()) {
+                const_cast<app::frame_data &>(read_frame).effect.count = 0;
+                
+                write_frame.particles_count = std::min(TOTAL_PARTICLES_COUNT, job_count.load());
+                
+                shared_frame_read_index = frame_read_index;
+
+                frame_write_index = acquire_available_frame_index();
+                shared_frame_write_index = frame_write_index;
+
+                release_frame_index(frame_write_index);
+
+                shared_effect_read_index = 0;
+
+                shared_particle_read_index = 0;
+                shared_particle_write_index = 0;
+
+                job_count = 0;
+
+                idle_workers = 0;
+
+                barrier->wait();
+            }
+
+            else barrier->wait();
+        }
+    }
+
+    std::uint32_t particle_engine::acquire_available_frame_index()
+    {
+        auto index = shared_frame_read_index.load();
+
+        do {
+            index = (index + 1) % FRAMES_COUNT;
+        } while (index == shared_frame_read_index.load() || acquired_indices.at(index).test_and_set());
+
+        return index;
+    }
+
+    std::uint32_t particle_engine::acquire_frame_index(std::uint32_t index)
+    {
+        acquired_indices.at(index).test_and_set();
+
+        return index;
+    }
+
+    std::uint32_t particle_engine::release_frame_index(std::uint32_t index)
+    {
+        acquired_indices.at(index).clear();
+
+        return index;
+    }
+
+    void particle_engine::update_worker_time_points(app::worker_context &worker_context)
+    {
+        thread_local static std::int64_t last_time = 0;
+
+        auto global_time = global_timer.load();
+        worker_context.dt = global_time - last_time;
+        last_time = global_time;
+
+        worker_context.from_start_time += std::chrono::milliseconds{worker_context.dt};
+    }
+
+    void particle_engine::process_particles(app::worker_context &worker_context)
+    {
+        auto &&generator = worker_context.generator;
+        auto &&bernoulli_distribution = worker_context.bernoulli_distribution;
+        auto &&uniform_real_distribution = worker_context.uniform_real_distribution;
+
+        auto &&read_frame = *worker_context.read_frame;
+        auto &&write_frame = *worker_context.write_frame;
+
+        auto is_dead = false;
+        auto is_outside = false;
+        auto is_exploded = false;
+
+        for (auto idx = shared_particle_read_index++; idx < read_frame.particles_count; idx = shared_particle_read_index++) {
+            auto &&read_particle = read_frame.particles.at(idx);
+
+            auto elapsed = static_cast<float>((worker_context.from_start_time - read_particle.born_time).count());
+            auto life_time = static_cast<float>(PARTICLE_LIFE_TIME.count()) * (uniform_real_distribution(generator) * .5f + .5f);
+
+            //is_dead = worker_context.from_start_time - read_particle.born_time > PARTICLE_LIFE_TIME;
+            is_dead = elapsed > life_time;
+            is_outside = is_particle_outside(read_particle);
+            is_exploded = !is_outside && is_dead && bernoulli_distribution(generator);
+
+            if (!is_outside & !is_dead) {
+                auto job_write_index = shared_particle_write_index++;
+
+                if (job_write_index >= TOTAL_PARTICLES_COUNT)
+                    continue;
+
+                auto &&write_particle = write_frame.particles.at(job_write_index);
+
+                write_particle.born_time = read_particle.born_time;
+                write_particle.time_stamp = worker_context.from_start_time;
+                write_particle.color = read_particle.color;
+
+                auto dt = static_cast<float>(worker_context.dt) * 1e-3f;
+
+                write_particle.position.at(0) = read_particle.position.at(0) + read_particle.velocity.at(0) * dt;
+                write_particle.position.at(1) = read_particle.position.at(1) + read_particle.velocity.at(1) * dt;
+
+                write_particle.velocity.at(0) = read_particle.velocity.at(0) * C_DRAG;
+                write_particle.velocity.at(1) = read_particle.velocity.at(1) * C_DRAG - G_ACCEL * dt;
+
+                ++job_count;
+            }
+
+            else if (is_exploded) {
+                auto j = shared_particle_write_index++;
+
+                for (auto i = 0u; j < TOTAL_PARTICLES_COUNT; ++i, j = shared_particle_write_index++) {
+                    if (i >= PER_EFFECT_PARTICLES_COUNT)
+                        break;
+
+                    auto &&particle = write_frame.particles.at(j);
+
+                    particle.born_time = worker_context.from_start_time;
+
+                    particle.position = read_particle.position;
+                    particle.color = read_particle.color;
+
+                    randomize_velocity_vector(worker_context, particle.velocity);
+
+                    ++job_count;
+                }
+            }
+        }
+    }
+
+    void particle_engine::add_particles(app::worker_context &worker_context)
+    {
+        auto &&read_frame = *worker_context.read_frame;
+        auto &&write_frame = *worker_context.write_frame;
+
+        auto &&position = read_frame.effect.position;
+        auto &&color = read_frame.effect.color;
+
+        auto j = 0u;
+
+        for (auto i = shared_effect_read_index++; i < read_frame.effect.count; i = shared_effect_read_index++) {
+            j = shared_particle_write_index++;
+
+            if (j >= TOTAL_PARTICLES_COUNT)
+                break;
+
+            auto &&write_particle = write_frame.particles.at(j);
+
+            write_particle.born_time = worker_context.from_start_time;
+
+            write_particle.position = position;
+            write_particle.color = color;
+
+            randomize_velocity_vector(worker_context, write_particle.velocity);
+
+            ++job_count;
+        }
+    }
+
+    void particle_engine::randomize_velocity_vector(app::worker_context &worker_context, app::vec2 &velocity)
+    {
+        auto &&generator = worker_context.generator;
+        auto &&uniform_real_distribution = worker_context.uniform_real_distribution;
+
+        auto angle = uniform_real_distribution(generator) * kPI * 2.f;
+        auto speed = (uniform_real_distribution(generator) * .75f + .25f) * 100.f;
+
+        velocity.at(0) = std::cos(angle) * speed;
+        velocity.at(1) = std::sin(angle) * speed;
+    }
+
+    bool particle_engine::is_particle_outside(app::particle const &particle)
+    {
+        auto &&position = particle.position;
+        return position.at(0) < 0 || position.at(0) > app::SCREEN_WIDTH || position.at(1) < 0 || position.at(1) > app::SCREEN_HEIGHT;
+    }
+}
---
+++ particle-engine/src/particle_engine.hxx
@@ -0,0 +1,144 @@
+#pragma once
+
+#include <atomic>
+#include <vector>
+#include <random>
+#include <array>
+
+#include "utility/barrier.hxx"
+
+
+namespace app
+{
+    auto constexpr EFFECTS_COUNT = 2048u;
+    auto constexpr PER_EFFECT_PARTICLES_COUNT = 64u;
+    auto constexpr TOTAL_PARTICLES_COUNT = EFFECTS_COUNT * PER_EFFECT_PARTICLES_COUNT;
+    auto constexpr DEAD_PARTICLE_EXPLOSION_CHANCE = .25f;
+    
+    using vec2 = std::array<float, 2>;
+    using vec4 = std::array<float, 4>;
+    
+    struct particle final {
+        std::chrono::milliseconds born_time{0}; // ms
+        std::chrono::milliseconds time_stamp{0}; // ms
+
+        vec2 position{0, 0};
+        vec2 velocity{0, 0};
+        vec4 color{0, 0, 0, 0};
+    };
+
+    struct effect final {
+        std::uint32_t count{0};
+
+        vec2 position{0, 0};
+        vec4 color{0, 0, 0, 0};
+    };
+
+    struct frame_data final {
+        frame_data() : particles_count{0}
+        {
+            particles.resize(TOTAL_PARTICLES_COUNT);
+        }
+
+        std::uint32_t particles_count;
+
+        std::vector<app::particle> particles;
+        app::effect effect;
+    };
+}
+
+namespace app
+{
+    struct worker_context final {
+        app::frame_data const *read_frame{nullptr};
+        app::frame_data *write_frame{nullptr};
+
+        std::chrono::milliseconds elapsed_time{0};
+        std::chrono::milliseconds from_start_time{0};
+
+        std::uint32_t worker_index;
+
+        std::random_device random_device;
+        std::mt19937 generator;
+
+        std::bernoulli_distribution bernoulli_distribution;
+        std::uniform_real_distribution<float> uniform_real_distribution;
+
+        std::uint64_t dt{0};
+
+        worker_context(std::uint32_t worker_index) : worker_index{worker_index}
+        {
+            generator = std::mt19937{random_device()};
+
+            bernoulli_distribution = std::bernoulli_distribution{DEAD_PARTICLE_EXPLOSION_CHANCE};
+            uniform_real_distribution = std::uniform_real_distribution<float>{0.f, 1.f};
+        }
+    };
+}
+
+namespace app
+{
+    class particle_engine final {
+    public:
+
+        particle_engine();
+
+        ~particle_engine();
+
+        void render();
+
+        void update(std::int64_t dt);
+
+        void spawn_effect(app::vec2 &&position, app::vec4 &&color);
+
+    private:
+
+        static std::chrono::milliseconds constexpr UPDATE_PERIOD{5};
+        static std::chrono::milliseconds constexpr PARTICLE_LIFE_TIME{2'000};
+
+        static auto constexpr FRAMES_COUNT{4u};
+
+        static auto constexpr C_DRAG{.999f};
+        static auto constexpr G_ACCEL{9.81f};
+
+        std::atomic_int64_t global_timer{0};
+
+        std::atomic_bool stop_workers;
+
+        std::array<std::atomic_flag, FRAMES_COUNT> acquired_indices;
+
+        std::vector<app::frame_data> frames_data;
+
+        std::vector<std::thread> workers;
+
+        std::atomic_uint32_t shared_frame_read_index{0};
+        std::atomic_uint32_t shared_frame_write_index{1};
+
+        std::atomic_uint32_t shared_effect_read_index{0};
+
+        std::atomic_uint32_t shared_particle_read_index{0};
+        std::atomic_uint32_t shared_particle_write_index{0};
+
+        std::atomic_uint32_t idle_workers{0};
+
+        std::atomic_uint32_t job_count{0};
+
+        std::unique_ptr<utility::barrier> barrier;
+
+        void worker_object(std::uint32_t worker_index);
+
+        void update_worker_time_points(app::worker_context &worker_context);
+
+        void process_particles(app::worker_context &worker_context);
+
+        void add_particles(app::worker_context &worker_context);
+
+        std::uint32_t acquire_available_frame_index();
+        std::uint32_t acquire_frame_index(std::uint32_t index);
+        std::uint32_t release_frame_index(std::uint32_t index);
+
+        void randomize_velocity_vector(app::worker_context &worker_context, app::vec2 &velocity);
+
+        static bool is_particle_outside(app::particle const &particle);
+    };
+}
